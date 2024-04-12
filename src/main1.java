bc8ec01ccc19235ad52674948c6cccb0b455d3f4
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2024-04-12 09:27:01 -0500

    se crean las diferentes instancias necesarias para el proyecto

---
+++ src/main1.java
@@ -0,0 +1,10 @@
+package src;
+
+public class main1 {
+    /**
+     *  se integra una clase de pruebas de objetos, entrada y salida de datos entre otros
+     */
+    public static void main(String[] args) {
+
+    }
+}
