a3ea3b12f12ba3a3b063acbb7d9654dd29270a7a
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2024-04-12 22:06:05 -0500

    generacion de los sockets, se crean las conexiones, user, servidor

---
+++ src/Networking/ArchivosClientes.java
@@ -0,0 +1,70 @@
+package src.Networking;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ArchivosClientes {
+    public class ClienteArchivos {
+
+        private Socket conector;
+        private String direccionIP;
+        private DataInputStream flujoEntrada;
+        private DataOutputStream flujoSalida;
+        private int puerto;
+
+        public ClienteArchivos(String direccionIP, int puerto) {
+            this.direccionIP = direccionIP;
+            this.puerto = puerto;
+        }
+
+        /**
+         *
+         * @return manda si se puede iniciar la conexion
+         */
+        public boolean iniciarConexionConServidor() {
+            try {
+                this.conector 		= new Socket(this.direccionIP, this.puerto);
+                this.flujoEntrada 	= new DataInputStream(this.conector.getInputStream());
+                this.flujoSalida 	= new DataOutputStream(conector.getOutputStream());
+
+                return true;
+            } catch(Exception e) {
+                return false;
+            } finally {
+               cerrarRecursos();
+            }
+        }
+
+        /**
+         *  se encarga de cerrar el flujo de datos. IMPORTANTE
+         */
+        public void cerrarRecursos() {
+            try {
+                this.flujoEntrada.close();
+                this.flujoSalida.close();
+                this.conector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public DataInputStream getFlujoEntrada() {
+            return flujoEntrada;
+        }
+
+        public void setFlujoEntrada(DataInputStream flujoEntrada) {
+            this.flujoEntrada = flujoEntrada;
+        }
+
+        public DataOutputStream getFlujoSalida() {
+            return flujoSalida;
+        }
+
+        public void setFlujoSalida(DataOutputStream flujoSalida) {
+            this.flujoSalida = flujoSalida;
+        }
+    }
+
+}
---
+++ src/Networking/ArchivosServidor.java
@@ -0,0 +1,67 @@
+package src.Networking;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.io.IOException;
+
+public class ArchivosServidor {
+    public class ClienteArchivos {
+
+        private Socket conector;
+        private String direccionIP;
+        private DataInputStream flujoEntrada;
+        private DataOutputStream flujoSalida;
+        private int puerto;
+
+        public ClienteArchivos(String direccionIP, int puerto) {
+            this.direccionIP = direccionIP;
+            this.puerto = puerto;
+        }
+
+        /**
+         * pone activo al servidor, es necesario para poder dar el tratamiento de datos
+         * @return si se pudo realizar la conexion o no.
+         */
+        public boolean iniciarConexionConServidor() {
+            try {
+                this.conector 		= new Socket(this.direccionIP, this.puerto);
+                this.flujoEntrada 	= new DataInputStream(this.conector.getInputStream());
+                this.flujoSalida 	= new DataOutputStream(conector.getOutputStream());
+
+                return true;
+            } catch(Exception e) {
+                return false;
+            } finally {
+                cerrarRecursos();
+            }
+        }
+
+        public void cerrarRecursos() {
+            try {
+                this.flujoEntrada.close();
+                this.flujoSalida.close();
+                this.conector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public DataInputStream getFlujoEntrada() {
+            return flujoEntrada;
+        }
+
+        public void setFlujoEntrada(DataInputStream flujoEntrada) {
+            this.flujoEntrada = flujoEntrada;
+        }
+
+        public DataOutputStream getFlujoSalida() {
+            return flujoSalida;
+        }
+
+        public void setFlujoSalida(DataOutputStream flujoSalida) {
+            this.flujoSalida = flujoSalida;
+        }
+    }
+
+}
